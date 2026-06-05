6a03ec73331df0dece2f81d7eadf4423ea1024f8
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 14:40:06 -0500

    Red

--- tests/test_parking.py
+++ tests/test_parking.py
@@ -8,3 +8,7 @@ def test_cobro_500_por_hora_o_fraccion_luego_de_30_min():
     assert calcular_tarifa(31) == 500
     assert calcular_tarifa(60) == 500
     assert calcular_tarifa(61) == 1000
+
+def test_tope_maximo_diario_12000():
+    assert calcular_tarifa(1440) == 12000 # 24 horas = (1410 min cobrables = 23.5h -> 24h cobradas = 12000)
+    assert calcular_tarifa(2000) == 12000
