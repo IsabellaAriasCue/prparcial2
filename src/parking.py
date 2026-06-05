c3e05624ca0db6398f35700941a6a58698e8266e
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 14:38:41 -0500

    Green

--- src/parking.py
+++ src/parking.py
@@ -1,2 +1,10 @@
+import math
+
+
 def calcular_tarifa(minutos: int) -> float:
-    return 0
+    if minutos <= 30:
+        return 0
+
+    # Las horas o fracciones se calculan sobre el tiempo TOTAL de estadía
+    horas = math.ceil(minutos / 60)
+    return horas * 500
