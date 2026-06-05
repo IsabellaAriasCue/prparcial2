e86cbc0eb8eb1da318584e513878a9cd35affdc6
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 14:33:29 -0500

    Red

--- tests/test_parking.py
+++ tests/test_parking.py
@@ -3,3 +3,8 @@ from src.parking import calcular_tarifa
 def test_primeros_30_minutos_son_gratis():
     assert calcular_tarifa(0) == 0
     assert calcular_tarifa(30) == 0
+
+def test_cobro_500_por_hora_o_fraccion_luego_de_30_min():
+    assert calcular_tarifa(31) == 500
+    assert calcular_tarifa(60) == 500
+    assert calcular_tarifa(61) == 1000
